e7a094390808c77636f58f1269bf4ae71c044a73
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-05-19 12:56:46 +0100

    Update added and form made into partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,12 +11,25 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
